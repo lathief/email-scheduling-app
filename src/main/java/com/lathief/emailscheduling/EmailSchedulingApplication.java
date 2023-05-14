155fee0aaa46c182061514f24cf93a641d45f7e9
lathief <shadowmaster241099@yahoo.co.id>
2023-05-14 12:50:51 +0700

    first commit

---
+++ src/main/java/com/lathief/emailscheduling/EmailSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.lathief.emailscheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSchedulingApplication.class, args);
+	}
+
+}
