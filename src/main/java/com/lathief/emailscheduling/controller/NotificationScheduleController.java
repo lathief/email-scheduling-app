2663060caed6479b8353480be0715cf5df376154
lathief <shadowmaster241099@yahoo.co.id>
2023-05-15 19:10:07 +0700

    make notification endpoin

---
+++ src/main/java/com/lathief/emailscheduling/controller/NotificationScheduleController.java
@@ -0,0 +1,44 @@
+package com.lathief.emailscheduling.controller;
+
+import com.lathief.emailscheduling.payload.request.PushNotificationRequest;
+import com.lathief.emailscheduling.payload.response.PushNotificationResponse;
+import com.lathief.emailscheduling.service.PushNotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/jobs/notification")
+public class NotificationScheduleController {
+    @Autowired
+    PushNotificationService pushNotificationService;
+    @PostMapping("/broadcast")
+    public ResponseEntity<?> sendNotificationToAll(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationWithoutData(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+    @PostMapping("/topic")
+    public ResponseEntity<?> sendNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationWithoutData(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @PostMapping("/token")
+    public ResponseEntity<?> sendTokenNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationToToken(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @PostMapping("/data")
+    public ResponseEntity<?> sendDataNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotification(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> sendSampleNotification() {
+        pushNotificationService.sendSamplePushNotification();
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+}
